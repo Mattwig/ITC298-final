5b7ff8930064330b0ee1b19ff115e333ca742075
cluetrain <greenstrat67@gmail.com>
2015-05-14 18:57:26 -0700

    Setup

---
+++ index.js
@@ -0,0 +1,14 @@
+var hapi = require("hapi");
+var server = new hapi.Server();
+
+server.connection({port:8000});
+
+server.start();
+
+server.route({
+    method:"GET",
+    path:'/',
+    handler: function(req, reply){
+    reply('hello world')
+    }
+});

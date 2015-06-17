8ce8301b8d7ad0e3b76f179eb1fe4e50e4208ac0
Dan Mattwig <dmattwig@gmail.com>
2015-06-16 22:22:04 -0700

    Add hapi auth

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
 var hapi = require("hapi");
-var server = new hapi.Server();
+var basic = require("hapi-auth-basic");
 var Recipe = require("./models/recipe");
 var async = require("async");
 var db = require("./db");
+var Bcrypt = require("bcrypt")
 
+
+var server = new hapi.Server();
 server.connection({port:8000});
+
+var users = {
+    admin: {
+        username: 'admin',
+        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+        name: 'admin',
+        id: '2'
+    }
+};
+
+
+var validate = function (username, password, callback) {
+  var user = users[username];
+  if (!user) {
+    console.log("nope");
+    return callback(null, false);
+  }
+  Bcrypt.compare(password, user.password, function(err, isValid){
+    console.log(isValid);
+    callback(err, isValid, {id: user.id, name: user.name});
+  });
+};
+
+
+server.register(basic, function(err){
+  server.auth.strategy('simple', 'basic', {validateFunc: validate});
+  server.route({
+    method: 'GET',
+    path: '/auth',
+    config: {
+      auth: 'simple',
+      handler: function(req, reply){
+        reply("hello, " +request.auth.credientals.name);
+      }
+    }
+  });
+});
+
+
+
 
 db.init(function(err) {
   if (err) {
-    console.log(err)
+    console.log(err);
     return console.error("broken");
   }
   console.log("Database ready, starting server...");
